58ece4ce6c8bd5d7661bd441b23aefeef2ccce49
Brodie Brock <brodiebrock@gmail.com>
2022-02-03 23:11:34 -0600

    created user login reducers and constants

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,9 +1,10 @@
 import { createStore, combineReducers, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
 import { composeWithDevTools } from "redux-devtools-extension";
+import { userLoginReducer } from "./reducers/userReducers";
 
 const reducer = combineReducers({
-  //this will contain reducers
+  userLogin: userLoginReducer,
 });
 
 const initialState = {};
