fecfea5b50096d8a7f6dc4ecce7012026ad556d5
Brodie Brock <brodiebrock@gmail.com>
2022-02-07 16:25:42 -0600

    adding logout with react-redux

--- frontend/src/components/header/header.js
+++ frontend/src/components/header/header.js
@@ -6,12 +6,23 @@ import {
   Navbar,
   NavDropdown,
 } from "react-bootstrap";
-
 import React from "react";
 import { Link, useHistory } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { logout } from "../../actions/userActions";
 
 const Header = () => {
   const history = useHistory();
+
+  const dispatch = useDispatch();
+
+  const userLogin = useSelector((state) => state.userLogin);
+  const { userInfo } = userLogin;
+
+  const logoutHandler = () => {
+    dispatch(logout());
+    history.push("/");
+  };
   return (
     <Navbar bg="primary" expand="lg" varient="darl">
       <Container>
@@ -35,12 +46,7 @@ const Header = () => {
             </Nav.Link>
             <NavDropdown title="Settings" id="basic-nav-dropdown">
               <NavDropdown.Item href="#action/3.1">My profile</NavDropdown.Item>
-              <NavDropdown.Item
-                onClick={() => {
-                  localStorage.removeItem("userInfo");
-                  history.push("/");
-                }}
-              >
+              <NavDropdown.Item onClick={logoutHandler}>
                 Logout
               </NavDropdown.Item>
             </NavDropdown>
