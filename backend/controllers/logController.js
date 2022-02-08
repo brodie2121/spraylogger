c2f3c2b026543436ae42f79fbfcf07390ad18f41
Brodie Brock <brodiebrock@gmail.com>
2022-02-08 16:05:11 -0600

    created the createLog API

--- backend/controllers/logController.js
+++ backend/controllers/logController.js
@@ -2,8 +2,39 @@ const Log = require("../models/logModel");
 const asyncHandler = require("express-async-handler");
 
 const getLogs = asyncHandler(async (req, res) => {
-  const logs = await Log.find();
+  const logs = await Log.find({ user: req.user._id });
   res.json(logs);
 });
 
-module.exports = { getLogs };
+const createLog = asyncHandler(async (req, res) => {
+  const { date_applied, operator, location, holes_treated, chemicals, notes } =
+    req.body;
+
+  if (
+    !date_applied ||
+    !operator ||
+    !location ||
+    !holes_treated ||
+    !chemicals ||
+    !notes
+  ) {
+    res.status(400);
+    throw new Error("Please fill in the fields");
+  } else {
+    const log = new Log({
+      user: req.user._id,
+      date_applied,
+      operator,
+      location,
+      holes_treated,
+      chemicals,
+      notes,
+    });
+
+    const createdLog = await log.save();
+
+    res.status(201).json(createdLog);
+  }
+});
+
+module.exports = { getLogs, createLog };
--- backend/routes/logRoutes.js
+++ backend/routes/logRoutes.js
@@ -1,15 +1,10 @@
 const express = require("express");
-const { getLogs } = require("../controllers/logController");
+const { getLogs, createLog } = require("../controllers/logController");
 
 const { protect } = require("../middleware/authMiddleware.js");
 const router = express.Router();
 
 router.route("/").get(protect, getLogs);
-
-//router.route("/:id");
-//.get(getLogById)
-//.delete(protect, DeleteLog)
-//.put(protect, UpdateLog);
-//router.route("/create").post(protect, CreateLog);
+router.route("/create").post(protect, createLog);
 
 module.exports = router;
